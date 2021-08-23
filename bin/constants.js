b527a19c92894525f58d0f4d8491f8bb9e04ce2d
23n21 <zenzile.sithole@gmail.com>
2021-08-23 17:38:45 +0200

    chore: added org name

--- bin/constants.js
+++ bin/constants.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const ORG = process.env.ORG || 'strapi';
+const ORG = process.env.ORG || 'cluuza';
 const REPO = 'strapi/strapi';
 const BASE_IMAGE_NAME = `${ORG}/base`;
 const STRAPI_IMAGE_NAME = `${ORG}/strapi`;
-const NODE_VERSIONS = [10, 12, 14];
-const LATEST_NODE_VERSION = 14;
+const NODE_VERSIONS = [14, 16];
+const LATEST_NODE_VERSION = 16;
 
 module.exports = {
   ORG,
